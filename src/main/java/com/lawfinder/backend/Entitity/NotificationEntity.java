84d41d63fc48328e522a80f46eda4e6f29386823
unknown <alejandro.flores.a@ucb.edu.bo>
2023-05-14 15:52:11 -0400

    Person and Notification entities completed

--- src/main/java/com/lawfinder/backend/Entitity/NotificationEntity.java
+++ src/main/java/com/lawfinder/backend/Entitity/NotificationEntity.java
@@ -9,7 +9,69 @@ public class NotificationEntity {
     @Column(name = "NOTIFICATION_ID")
     private Long notificationId;
 
+    @Column(name = "ISREAD", nullable = false)
+    private boolean isRead;
+
+    @ManyToOne
+    @JoinColumn(name = "SUBCATEGORY_ID", nullable = false)
+    private SubCategoryEntity subCategory;
+
+    @ManyToOne
+    @JoinColumn(name = "CONTENT_NOTIFICATION_ID", nullable = false)
+    private ContentNotificationEntity contentNotification;
+
+    // Constructor vacio
+    public NotificationEntity() {}
+
+    // Constructor con todos los atributos
+
+    public NotificationEntity(boolean isRead, SubCategoryEntity subCategory, ContentNotificationEntity contentNotification) {
+        this.isRead = isRead;
+        this.subCategory = subCategory;
+        this.contentNotification = contentNotification;
+    }
+
+    // Getters
     
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public SubCategoryEntity getSubCategory() {
+        return subCategory;
+    }
+
+    public ContentNotificationEntity getContentNotification() {
+        return contentNotification;
+    }
+
+    // Setters
+
+    public void setNotificationId(Long notificationId) {
+        this.notificationId = notificationId;
+    }
     
+    public void setRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public void setSubCategory(SubCategoryEntity subCategory) {
+        this.subCategory = subCategory;
+    }
     
+    public void setContentNotification(ContentNotificationEntity contentNotification) {
+        this.contentNotification = contentNotification;
+    }
+    
+    // toString
+
+    @Override
+    public String toString() {
+        return "NotificationEntity [contentNotification=" + contentNotification + ", isRead=" + isRead
+                + ", notificationId=" + notificationId + ", subCategory=" + subCategory + "]";
+    }
 }
--- src/main/java/com/lawfinder/backend/Entitity/PersonEntity.java
+++ src/main/java/com/lawfinder/backend/Entitity/PersonEntity.java
@@ -15,9 +15,87 @@ public class PersonEntity {
     @Column(name = "LASTNAME")
     private String lastname;
 
+    @Column(name = "NUMBER", length = 100)
+    private String number;
+
+    @Column(name = "EMAIL", length = 300)
+    private String email;
+
     @ManyToOne
     @JoinColumn(name = "ADRESS_ID", referencedColumnName = "ADRESS_ID")
     private AddressEntity address;
+
+    // Constructor
+    public PersonEntity() {}
+    // Constructor con todos los atributos
+
+    public PersonEntity(String name, String lastname, String number, String email, AddressEntity address){
+        this.name = name;
+        this.lastname = lastname;
+        this.number = number;
+        this.email = email;
+        this.address = address;
+    }
+
+    // Getters
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public AddressEntity getAddress() {
+        return address;
+    }
+
+    // Setters
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(AddressEntity address) {
+        this.address = address;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "PersonEntity [address=" + address + ", email=" + email + ", lastname=" + lastname + ", name=" + name
+                + ", number=" + number + ", personId=" + personId + "]";
+    }
 
     
 }
