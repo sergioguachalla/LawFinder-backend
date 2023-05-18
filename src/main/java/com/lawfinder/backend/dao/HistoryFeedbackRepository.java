cf79b861eb15ff903f1f1b28d82dcbe4f2075092
unknown <alejandro.flores.a@ucb.edu.bo>
2023-05-18 15:00:00 -0400

    History legalcase, user and feedback added

---
+++ src/main/java/com/lawfinder/backend/dao/HistoryFeedbackRepository.java
@@ -0,0 +1,5 @@
+package com.lawfinder.backend.dao;
+import com.lawfinder.backend.Entity.HistoryFeedbackEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoryFeedbackRepository extends JpaRepository<HistoryFeedbackEntity,Long> {}
