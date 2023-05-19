b961c3848f945ee711898aa5a876adcd0cc96f0e
unknown <alejandro.flores.a@ucb.edu.bo>
2023-05-19 08:51:27 -0400

    Feedback and JurisFeedback added

---
+++ src/main/java/com/lawfinder/backend/Entity/FeedbackEntity.java
@@ -0,0 +1,90 @@
+package com.lawfinder.backend.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "FEEDBACK")
+public class FeedbackEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "FEEDBACK_ID")
+    private Long feedbackId;
+
+    @Column(name = "CONTENT", nullable = false, columnDefinition = "text")
+    private String content;
+
+    @Column(name = "USER_ID", nullable = false)
+    private int userId;
+
+    @Column(name = "TX_USER", nullable = false, length = 100)
+    private String txUser;
+
+    @Column(name = "TX_HOST", nullable = false, length = 100)
+    private String txHost;
+
+    @Column(name = "TX_DATE", nullable = false, length = 100)
+    private String txDate;
+
+    // Constructor
+    public FeedbackEntity() {
+    }
+
+    // Constructor con todos los atributos
+    public FeedbackEntity(String content, int userId, String txUser, String txHost, String txDate) {
+        this.content = content;
+        this.userId = userId;
+        this.txUser = txUser;
+        this.txHost = txHost;
+        this.txDate = txDate;
+    }
+
+    // Getters y setters
+
+    public Long getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(Long feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTxUser() {
+        return txUser;
+    }
+
+    public void setTxUser(String txUser) {
+        this.txUser = txUser;
+    }
+
+    public String getTxHost() {
+        return txHost;
+    }
+
+    public void setTxHost(String txHost) {
+        this.txHost = txHost;
+    }
+
+    public String getTxDate() {
+        return txDate;
+    }
+
+    public void setTxDate(String txDate) {
+        this.txDate = txDate;
+    }
+}
---
+++ src/main/java/com/lawfinder/backend/Entity/JurisprudenceFeedbackEntity.java
@@ -0,0 +1,80 @@
+package com.lawfinder.backend.Entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "JURISPRUDENCE_FEEDBACK")
+public class JurisprudenceFeedbackEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "JURISPRUDENCE_FEEDBACK_ID")
+    private Long jurisprudenceFeedbackId;
+
+    @Column(name = "FEEDBACK_ID", nullable = false)
+    private FeedbackEntity feedbackId;
+
+    @Column(name = "JURISPRUDENCE_ID", nullable = false)
+    private JurisprudenceEntity jurisprudenceId;
+
+    @Column(name = "DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    // Constructor
+    public JurisprudenceFeedbackEntity() {
+    }
+
+    // Constructor con todos los atributos
+    public JurisprudenceFeedbackEntity(FeedbackEntity feedbackId, JurisprudenceEntity jurisprudenceId, Date date) {
+        this.feedbackId = feedbackId;
+        this.jurisprudenceId = jurisprudenceId;
+        this.date = date;
+    }
+
+    // Getters y setters
+
+    public Long getJurisprudenceFeedbackId() {
+        return jurisprudenceFeedbackId;
+    }
+
+    public void setJurisprudenceFeedbackId(Long jurisprudenceFeedbackId) {
+        this.jurisprudenceFeedbackId = jurisprudenceFeedbackId;
+    }
+
+    public FeedbackEntity getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(FeedbackEntity feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public JurisprudenceEntity getJurisprudenceId() {
+        return jurisprudenceId;
+    }
+
+    public void setJurisprudenceId(JurisprudenceEntity jurisprudenceId) {
+        this.jurisprudenceId = jurisprudenceId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "JurisprudenceFeedbackEntity{" +
+                "jurisprudenceFeedbackId=" + jurisprudenceFeedbackId +
+                ", feedbackId=" + feedbackId +
+                ", jurisprudenceId=" + jurisprudenceId +
+                ", date=" + date +
+                '}';
+    }
+}
