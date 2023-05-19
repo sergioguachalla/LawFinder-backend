75cd1883ad9035fbc6ff2d96e9349a585bd1e083
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-05-18 21:10:16 -0400

    cambios

--- src/main/java/com/lawfinder/backend/bl/DepartmentBl.java
+++ src/main/java/com/lawfinder/backend/bl/DepartmentBl.java
@@ -2,6 +2,7 @@ package com.lawfinder.backend.bl;
 
 import com.lawfinder.backend.Entity.DepartmentEntity;
 import com.lawfinder.backend.dao.DepartmentRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,5 +25,9 @@ public class DepartmentBl {
     public List<DepartmentEntity> findByDepartmentId(Long departmentId){
         return departmentRepository.findByDepartmentId(departmentId);
     }
+    /*@Query("SELECT d FROM DEPARTMENT d WHERE d.department_id = departmentId")
+    public List<DepartmentEntity> findAllByDepartmentId(Long departmentId){
+
+    }*/
 
 }
