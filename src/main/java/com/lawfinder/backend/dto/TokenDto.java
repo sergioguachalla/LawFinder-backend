bce041c713a750ff9fb0d65ae037a2c2ca1b6304
unknown <alejandro.flores.a@ucb.edu.bo>
2023-05-19 22:43:20 -0400

    TokenDto added

---
+++ src/main/java/com/lawfinder/backend/dto/TokenDto.java
@@ -0,0 +1,44 @@
+package com.lawfinder.backend.dto;
+
+public class TokenDto {
+    private String authToken;
+    private String refreshToken;
+
+
+    public TokenDto() {}
+
+    // constructor
+
+    public TokenDto(String authToken, String refreshToken) {
+        this.authToken = authToken;
+        this.refreshToken = refreshToken;
+    }
+    
+
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getRefreshToken() {
+        return this.refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " authToken='" + getAuthToken() + "'" +
+            ", refreshToken='" + getRefreshToken() + "'" +
+            "}";
+    }
+
+
+}
