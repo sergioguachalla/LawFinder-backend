28fff01211cec3b74baeb7f05ad37ff4cd67fda7
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-05-23 00:02:28 -0400

    ERROR: UserRepository findByUsername method

--- src/main/java/com/lawfinder/backend/dao/UserRepository.java
+++ src/main/java/com/lawfinder/backend/dao/UserRepository.java
@@ -2,9 +2,14 @@ package com.lawfinder.backend.dao;
 
 import com.lawfinder.backend.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
-    UserEntity findByUsername(String username);
+    @Query("SELECT u FROM UserEntity u WHERE u.username = :username")
+    UserEntity findUserEntityByUsername(@Param("username") String username);
 
 }
