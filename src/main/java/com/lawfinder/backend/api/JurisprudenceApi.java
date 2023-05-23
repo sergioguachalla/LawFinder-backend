174bb4696b6f7dfca0790a25964a9e6f82b39148
unknown <alejandro.flores.a@ucb.edu.bo>
2023-05-23 13:14:49 -0400

    jurispridence api works

--- src/main/java/com/lawfinder/backend/api/JurisprudenceApi.java
+++ src/main/java/com/lawfinder/backend/api/JurisprudenceApi.java
@@ -3,13 +3,12 @@ package com.lawfinder.backend.api;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.lawfinder.backend.bl.DepartmentBl;
 import com.lawfinder.backend.dto.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import com.lawfinder.backend.bl.*;
 
 @RestController
@@ -36,6 +35,7 @@ public class JurisprudenceApi {
 
         
     }
+
 
     
 }
