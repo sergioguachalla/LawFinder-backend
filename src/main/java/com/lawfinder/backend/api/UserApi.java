e3b53e0457abbac2b552d62f62467f1e8fc3688c
unknown <alejandro.flores.a@ucb.edu.bo>
2023-05-31 18:07:53 -0400

    cors added to apis

--- src/main/java/com/lawfinder/backend/api/UserApi.java
+++ src/main/java/com/lawfinder/backend/api/UserApi.java
@@ -1,1 +1,68 @@
+package com.lawfinder.backend.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import com.lawfinder.backend.bl.*;
+import com.lawfinder.backend.dto.*;
+@CrossOrigin(origins = "*")
+
+@RestController
+public class UserApi {
+    @Autowired
+    private UserBl userBl;
+    @Autowired
+    private PersonBl personBl;
+    // Constructor
+
+    public UserApi(UserBl userBl, PersonBl personBl) {
+        this.userBl = userBl;
+        this.personBl = personBl;
+    }
+/* 
+    @GetMapping("/api/v1/user")
+    public ResponseDto<List<UserDto>> getAllTasks(
+        /*@RequestHeader("Authorization") String token) {
+         AuthBl authBl = new AuthBl();
+        if (!authBl.validateToken(token)) {
+            ResponseDto<List<UserDto>> response = new ResponseDto<>();
+            response.setCode("0001");
+            response.setResponse(null);
+            response.setErrorMessage("Invalid token");
+            return response;
+        }
+        ResponseDto<List<UserDto>> response = new ResponseDto<>();
+        response.setCode("0000");
+        response.setResponse(this.userBl.findAll());
+        return response;
+    }
+*/
+    @PostMapping("/api/v1/user")
+    public ResponseDto<String> createUser(@RequestBody UserDto user /* , @RequestHeader("Authorization") String token*/) {
+        ResponseDto<String> response = new ResponseDto<>();
+       /*  AuthBl authBl = new AuthBl();
+        if (!authBl.validateToken(token)) {
+            response.setCode("0001");
+            response.setResponse(null);
+            response.setErrorMessage("Invalid token");
+            return response;
+        }
+        
+        */
+        System.out.println(user.toString());
+        this.userBl.saveUser(user);;
+        response.setCode("0000");
+        response.setResponse("user created");
+        return response;
+
+        
+    }
+
+
+
+}
