a98ea362cf8c8afc2ae8784bb3826efb6683bbbf
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-06-06 16:55:30 -0400

    cambios finales presentación 06/06/2023 funciona todo

--- src/main/java/com/lawfinder/backend/dao/LegalCaseRepository.java
+++ src/main/java/com/lawfinder/backend/dao/LegalCaseRepository.java
@@ -8,19 +8,28 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface LegalCaseRepository extends  JpaRepository<LegalCaseEntity,Long>{
-    /*@Query(value = "select * from legal_case where user_id = :userId",nativeQuery = true)
-    List<LegalCaseEntity> findAllByUserId(@Param("userId") Long userId);*/
 
-   @Query(value = """
+
+public interface LegalCaseRepository extends  JpaRepository<LegalCaseEntity,Long>{
+   /*@Query(value = """
            SELECT *
            FROM LEGAL_CASE
            WHERE USER_ID = :userId
               OR LEGAL_CASE_ID IN (SELECT LEGAL_CASE_ID FROM ACTOR WHERE USER_ID = :userId)""",
 
-            countQuery = "SELECT COUNT(*) FROM legal_case lc, actor a WHERE lc.user_id = :userId or a.user_id = :user_id and status = true",
-            nativeQuery = true)
+           countQuery = "SELECT COUNT(*) FROM legal_case lc, actor a WHERE lc.user_id = :userId or a.user_id = :userId and status = true",
+           nativeQuery = true)
+   Page<LegalCaseEntity> findAllByUserId(@Param("userId") Long userId, Pageable pageable);*/
+   @Query(value = """
+           SELECT lc.*
+           FROM legal_case lc
+           WHERE lc.user_id = :userId
+              OR lc.legal_case_id IN (SELECT a.legal_case_id FROM actor a WHERE a.user_id = :userId and a.status = true)
+""",
+           countQuery = "SELECT COUNT(*) FROM legal_case lc, actor a WHERE lc.user_id = :userId or a.user_id = :userId and a.status = true",
+           nativeQuery = true)
    Page<LegalCaseEntity> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
+
 
    /*@Query(value = "SELECT lc.* FROM legal_case lc LEFT JOIN actor a ON lc.user_id = a.user_id WHERE lc.user_id = :userId OR a.user_id = :userId AND lc.status = true",
            countQuery = "SELECT COUNT(*) FROM legal_case lc LEFT JOIN actor a ON lc.user_id = a.user_id WHERE lc.user_id = :userId OR a.user_id = :userId AND lc.status = true",
@@ -29,5 +38,3 @@ public interface LegalCaseRepository extends  JpaRepository<LegalCaseEntity,Long
 */
 
 }
-
-
