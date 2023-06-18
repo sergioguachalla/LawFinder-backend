5c68c750eeb39c70a5ef0c6ff9b1bc5ebdef9a4b
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-18 17:38:31 -0400

    save audience works

--- src/main/java/com/lawfinder/backend/api/AudienceApi.java
+++ src/main/java/com/lawfinder/backend/api/AudienceApi.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AudienceApi {
-   // @Autowired
-   // private AudienceBl audienceBl;
+    @Autowired
+    private AudienceBl audienceBl;
 
     /* 
     @GetMapping("/api/v1/legalcase/{idCase}/audience")
@@ -35,16 +35,16 @@ public class AudienceApi {
 
     }*/
 
-   /*
+   
     @PostMapping("/api/v1/legalcase/{idCase}/audience")
     public ResponseDto<String> createAudience(@PathVariable Long idCase, @RequestBody AudienceDto audienceDto){
         ResponseDto<String> response = new ResponseDto<>();
-        //this.audienceBl.createAudience(idCase, audienceDto);
+        this.audienceBl.saveAudience(idCase, audienceDto);
         response.setCode("0000");
         response.setResponse("Audiencia creada");
         response.setErrorMessage(null);
         return response;
 
     }
-    */
+    
 }
--- src/main/java/com/lawfinder/backend/bl/AudienceBl.java
+++ src/main/java/com/lawfinder/backend/bl/AudienceBl.java
@@ -1,5 +1,31 @@
 package com.lawfinder.backend.bl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lawfinder.backend.Entity.AudienceEntity;
+import com.lawfinder.backend.Entity.InstanceLegalCaseEntity;
+import com.lawfinder.backend.dao.AudienceRepository;
+import com.lawfinder.backend.dto.AudienceDto;
+
+@Service
 public class AudienceBl {
+    @Autowired
+    private AudienceRepository audienceRepository;
+
+    //Save audience
+    public void saveAudience(Long idCase,AudienceDto audienceDto){
+        AudienceEntity audienceEntity = new AudienceEntity();
+        InstanceLegalCaseEntity instanceLegalCaseEntity = new InstanceLegalCaseEntity();
+        instanceLegalCaseEntity.setInstanceLegalCaseId(idCase);
+        audienceEntity.setAudienceDate(audienceDto.getAudienceDate());
+        audienceEntity.setDescription(audienceDto.getDescription());
+        audienceEntity.setLink(audienceDto.getLink());
+        audienceEntity.setAddress(audienceDto.getAddress());
+        audienceEntity.setInstanceLegalCase(instanceLegalCaseEntity);        
+        audienceRepository.saveAndFlush(audienceEntity);
+    }
+    
+
     
 }
