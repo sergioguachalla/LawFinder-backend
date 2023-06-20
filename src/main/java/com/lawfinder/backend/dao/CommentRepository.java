f9036d60cd05525a39f643c9c078b14c316ff3f8
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-19 20:41:09 -0400

    pagination added to comments

--- src/main/java/com/lawfinder/backend/dao/CommentRepository.java
+++ src/main/java/com/lawfinder/backend/dao/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.lawfinder.backend.dao;
 
 import com.lawfinder.backend.Entity.CommentEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +12,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
     @Query("SELECT c FROM CommentEntity c WHERE c.legalCaseId.legalCaseId = :legalCaseId")
-    List<CommentEntity> findByLegalCaseId(@Param("legalCaseId") Long legalCaseId);
+    Page<CommentEntity> findByLegalCaseId(@Param("legalCaseId") Long legalCaseId, Pageable pageable);
 
 }
