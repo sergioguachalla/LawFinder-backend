26114d35683256a76ef59e104031a008a49d2d34
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-19 23:00:54 -0400

    user image dao, and entity deleted

--- src/main/java/com/lawfinder/backend/dao/UserImageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lawfinder.backend.dao;
-import com.lawfinder.backend.Entity.UserImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserImageRepository extends JpaRepository<UserImageEntity,Long> {
-}
