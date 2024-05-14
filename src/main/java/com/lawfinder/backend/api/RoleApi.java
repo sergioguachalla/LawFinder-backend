3ec67c58b49bca652d0855d27f3638db9f295d8a
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-05-14 15:45:48 -0400

    add POST roles

---
+++ src/main/java/com/lawfinder/backend/api/RoleApi.java
@@ -0,0 +1,25 @@
+package com.lawfinder.backend.api;
+
+import com.lawfinder.backend.bl.RoleBl;
+import com.lawfinder.backend.dto.ResponseDto;
+import com.lawfinder.backend.dto.RoleDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/roles")
+public class RoleApi {
+    @Autowired private RoleBl roleBl;
+
+    @PostMapping("/")
+    public ResponseDto<String> saveRole(@RequestBody RoleDto roleDto){
+        roleBl.saveRole(roleDto);
+        return new ResponseDto<>("0001", "Role saved successfully", null);
+    }
+
+    @PostMapping("/privileges/")
+    public ResponseDto<String> addPrivilegeToRole(@RequestParam("roleId") Long roleId,@RequestParam("privilegeId") Long privilegeId) {
+        roleBl.addPrivilegeToRole(roleId, privilegeId);
+        return new ResponseDto<String>("0001", "Privilege added to role successfully", null);
+    }
+}
---
+++ src/main/java/com/lawfinder/backend/bl/RoleBl.java
@@ -0,0 +1,42 @@
+package com.lawfinder.backend.bl;
+
+import com.lawfinder.backend.Entity.PrivilegeEntity;
+import com.lawfinder.backend.Entity.PrivilegeRoleEntity;
+import com.lawfinder.backend.Entity.RoleEntity;
+import com.lawfinder.backend.dao.PrivilegeRepository;
+import com.lawfinder.backend.dao.PrivilegeRoleRepository;
+import com.lawfinder.backend.dao.RoleRepository;
+import com.lawfinder.backend.dto.RoleDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleBl {
+    @Autowired private RoleRepository roleRepository;
+    @Autowired private PrivilegeRoleRepository privilegeRoleRepository;
+    @Autowired private PrivilegeRepository privilegeRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(RoleBl.class);
+
+    public void saveRole(RoleDto roleDto){
+        RoleEntity roleEntity = new RoleEntity();
+        logger.info("RoleDto: {}", roleDto.getRoleName());
+        logger.info("RoleDto: {}", roleDto.getRoleId());
+        logger.info("RoleDto: {}", roleDto.toString());
+        roleEntity.setRoleName(roleDto.getRoleName());
+        roleEntity.setStatus(1);
+        roleRepository.save(roleEntity);
+    }
+
+    public void addPrivilegeToRole(Long roleId, Long privilegeId) {
+        PrivilegeEntity privilegeEntity = privilegeRepository.findById(privilegeId).orElseThrow();
+        RoleEntity roleEntity = roleRepository.findById(roleId).orElseThrow();
+        PrivilegeRoleEntity privilegeRoleEntity = new PrivilegeRoleEntity();
+        privilegeRoleEntity.setRole(roleEntity);
+        privilegeRoleEntity.setPrivilege(privilegeEntity);
+        privilegeRoleEntity.setStatus(1);
+        privilegeRoleRepository.save(privilegeRoleEntity);
+    }
+}
