9192583d9445c872bc5b26a90c12289991548a3c
unknown <alejandro.flores.a@ucb.edu.bo>
2024-05-27 17:10:34 -0400

    confidentiality fixed

--- src/main/java/com/lawfinder/backend/api/AssetsApi.java
+++ src/main/java/com/lawfinder/backend/api/AssetsApi.java
@@ -10,13 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.lawfinder.backend.bl.InformationAssetsClassificationBl;
+@CrossOrigin(origins = "*")
 
 @RestController
+
 @RequestMapping("/api/v1/informationAssets")
 public class AssetsApi {
 
